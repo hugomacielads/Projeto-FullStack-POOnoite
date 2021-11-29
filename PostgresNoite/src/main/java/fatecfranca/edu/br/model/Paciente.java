f27a5ccd7855bdb840ce01d3fd49d72b89d351a8
hugomacielads <hugomacielcesar@gmail.com>
2021-11-29 20:18:03 -0300

    (29/11) Commit Final

---
+++ PostgresNoite/src/main/java/fatecfranca/edu/br/model/Paciente.java
@@ -0,0 +1,78 @@
+package fatecfranca.edu.br.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // entidade do banco de dados relacional
+@Table(name="paciente") // classe relacionada com a tabela paciente
+public class Paciente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id; // chave primária com auto-incremento
+	
+	private String nome;
+	private String cpf;
+	private int idade;
+	private float peso;
+	private float altura;
+	
+	public Paciente() {
+		super();
+	}
+	public Paciente(Long id, String nome, String cpf, int idade, float peso, float altura) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	@Override
+	public String toString() {
+		return "Paciente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", idade=" + idade + ", peso=" + peso
+				+ ", altura=" + altura + "]";
+	}
+	
+	
+}
